2a4a7d449e8313e4fab7ca5d603a27e490d25f0e
senith <senithdakshina@gmail.com>
2025-12-08 22:55:17 +0530

    formated date

--- components/InterviewCard.tsx
+++ components/InterviewCard.tsx
@@ -1,14 +1,29 @@
-import React from 'react'
-import dayjs from 'dayjs';
-const InterviewCard = ({interviewid ,userid,role,type,techstack,createdAt} :InterviewCardProps) => {
-    const feedback = null as Feedback | null;
-    const normalizedType = /mix/gi.test(type) ? 'Mixed' : type;
-    const formattedate = dayjs(feedback?.createdAt || createdAt || Date.now()).format('MM D,YYYY');
+import React from "react";
+import dayjs from "dayjs";
+const InterviewCard = ({
+  interviewid,
+  userid,
+  role,
+  type,
+  techstack,
+  createdAt,
+}: InterviewCardProps) => {
+  const feedback = null as Feedback | null;
+  const normalizedType = /mix/gi.test(type) ? "Mixed" : type;
+  const formattedate = dayjs(
+    feedback?.createdAt || createdAt || Date.now()
+  ).format("MM D,YYYY");
   return (
-    <div className='card-border w-[350px] max-sm:w-full min-h-96'>
-      
+    <div className="card-border w-[350px] max-sm:w-full min-h-96">
+      <div className="card-interview">
+        <div>
+          <div>
+            <p className="badge-text"> {normalizedType}</p>
+            </div>
+        </div>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default InterviewCard
+export default InterviewCard;
