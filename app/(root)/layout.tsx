1e4554d4dad6a35976a558a3507d5c3074cdb3bb
senith <senithdakshina@gmail.com>
2026-01-17 10:12:04 +0530

    redirect corrected roo t o sign_in

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -1,24 +1,26 @@
-import { Mona_Sans } from "next/font/google";
-import React, { ReactNode } from 'react'
-import Image from "next/image";
 import Link from "next/link";
-const monaSans = Mona_Sans({
-  variable: "--font-mona-sans",
-  subsets: ["latin"],
-});
+import Image from "next/image";
+import { ReactNode } from "react";
+import { redirect } from "next/navigation";
 
-const Rootlayout = ({children} :{children: ReactNode}) => {
+import { isAuthenticated } from "@/lib/actions/auth.action ";
+
+const Layout = async ({ children }: { children: ReactNode }) => {
+  const isUserAuthenticated = await isAuthenticated();
+  if (!isUserAuthenticated) redirect("/sign_in");
+
   return (
     <div className="root-layout">
       <nav>
-        <Link  href="/" className="flex items-center gap-2">
-          <Image src="/Logo.svg" alt="Logo" width={38} height={32}></Image>
-          <h2 className="text-primary-100">VocaAI</h2>
+        <Link href="/" className="flex items-center gap-2">
+          <Image src="/logo.svg" alt="MockMate Logo" width={38} height={32} />
+          <h2 className="text-primary-100">PrepWise</h2>
         </Link>
       </nav>
+
       {children}
     </div>
-  )
-}
+  );
+};
 
-export default Rootlayout
+export default Layout;
